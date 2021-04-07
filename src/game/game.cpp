e7827a10802e0657f807198c475fb6894619819e
Diego S. Seabra <dss-diego@hotmail.com>
2021-04-07 20:47:23 -0300

    atlas animation tests

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -2,19 +2,24 @@
 // Created by Diego Santos Seabra on 26/03/21.
 //
 
-#include <glm/ext.hpp>
+#include <iostream>
 #include "game.h"
-#include "../engine/sprite_renderer.h"
 #include "../engine/resource_manager.h"
-#include "../engine/sprite.h"
 #include "../engine/scene.h"
+#include "../engine/renderer.h"
+#include "../engine/sprite_atlas.h"
 
-//SpriteRenderer *spriteRenderer;
+Renderer2D *renderer2D;
 
-//Sprite *coin;
-//Sprite *extraLife;
+SpriteAtlas marioAtlas;
+SpriteAtlas effectsAtlas;
+SpriteAtlas objectsAtlas;
 
-Scene testScene = Scene();
+int marioAnimIdx = 1;
+int coinAnimIdx = 8;
+int blockAnimIdx = 4;
+
+//Scene testScene = Scene();
 
 Game::Game(unsigned int width, unsigned int height) : state(ACTIVE), keys(), width(width), height(height)
 {}
@@ -26,6 +31,20 @@ Game::~Game()
 
 void Game::init()
 {
+//    ResourceManager::LoadTexture("assets/sprites/square_white.png", true, "square_white");
+//    ResourceManager::LoadTexture("assets/atlas/mario_atlas.png", true, "mario_atlas");
+//    ResourceManager::LoadTexture("assets/atlas/atlas_test.png", true, "atlas_test");
+
+    marioAtlas = SpriteAtlas(ResourceManager::LoadTexture("assets/atlas/atlas_mario.png", true, ""),
+                             16, 32, 13);
+
+    effectsAtlas = SpriteAtlas(ResourceManager::LoadTexture("assets/atlas/atlas_effects.png", true, ""),
+                               16, 16, 8);
+
+    objectsAtlas = SpriteAtlas(ResourceManager::LoadTexture("assets/atlas/atlas_objects.png", true, ""),
+                               16, 16, 19);
+
+    renderer2D = new Renderer2D();
 //    coin = new Sprite("assets/sprites/coin.png", true);
 //    extraLife = new Sprite("assets/sprites/extra_life.png", true);
 }
@@ -37,19 +56,32 @@ void Game::processInput(float deltaTime)
 
 void Game::update(float deltaTime)
 {
+//    std::cout << "DT: " << deltaTime << std::endl;
+    // NOTE: Animation tests. It should render sprites at a fixed timestep
+    if ((int) deltaTime % 6 == 0)
+    {
+        // Mario running
+        marioAnimIdx += 1;
+        if (marioAnimIdx > 2)
+            marioAnimIdx = 1;
 
+        // Coin spin
+        coinAnimIdx += 1;
+        if (coinAnimIdx > 11)
+            coinAnimIdx = 8;
+
+        // Block kick
+        blockAnimIdx += 1;
+        if (blockAnimIdx > 7)
+            blockAnimIdx = 4;
+    }
 }
 
 void Game::render()
 {
-//    coin->draw(glm::vec2(200.0f,200.0f), glm::vec2(48.0f, 48.0f),
-//                               0.0f, glm::vec3(1.0f, 1.0f, 1.0f));
-
-//    extraLife->draw(glm::vec2(250.0f, 250.0f), glm::vec2(48.0f, 48.0f), 0.0f,
-//                    glm::vec3(1.0f, 1.0f, 1.0f));
-
-//    spriteRenderer->drawSprite(ResourceManager::GetTexture("face"),
-//                               glm::vec2(200.0f, 200.0f), glm::vec2(128.0f, 32.0f),
-//                               0.0f, glm::vec3(1.0f, 1.0f, 1.0f));
-    // TODO: Create transform class
+    renderer2D->drawSprite(marioAtlas.getSprite(marioAnimIdx), glm::vec2(0.0f, 0.0f), glm::vec2(48.0f, 96.0f));
+    renderer2D->drawSprite(objectsAtlas.getSprite(coinAnimIdx), glm::vec2(64.0f, 0.0f), glm::vec2(48.0f, 48.0f));
+    renderer2D->drawSprite(objectsAtlas.getSprite(blockAnimIdx), glm::vec2(128.0f, 0.0f), glm::vec2(48.0f, 48.0f));
+//    renderer2D->drawQuad(glm::vec2(0.0f, 0.0f), glm::vec2(100.0f, 100.0f), Color::Cyan);
+//    renderer2D->drawSprite(ResourceManager::GetTexture("mario_atlas"), glm::vec2(0.0f, 0.0f), glm::vec2(544.0f, 416.0f));
 }
