94010f3a5774638ecfea504f8a06a162bf442352
Diego S. Seabra <dss-diego@hotmail.com>
2021-04-10 13:13:28 -0300

    card atlas rendering test

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -16,6 +16,8 @@ SpriteAtlas marioAtlas;
 SpriteAtlas effectsAtlas;
 SpriteAtlas objectsAtlas;
 
+SpriteAtlas cardAtlas;
+
 int marioAnimIdx = 1;
 int coinAnimIdx = 8;
 int blockAnimIdx = 4;
@@ -37,14 +39,17 @@ void Game::init()
 //    ResourceManager::LoadTexture("assets/atlas/mario_atlas.png", true, "mario_atlas");
 //    ResourceManager::LoadTexture("assets/atlas/atlas_test.png", true, "atlas_test");
 
-    marioAtlas = SpriteAtlas(ResourceManager::LoadTexture("assets/atlas/atlas_mario.png", true, ""),
-                             16, 32, 13);
+//    marioAtlas = SpriteAtlas(ResourceManager::LoadTexture("assets/atlas/atlas_mario.png"),
+//                             16, 32);
 
-    effectsAtlas = SpriteAtlas(ResourceManager::LoadTexture("assets/atlas/atlas_effects.png", true, ""),
-                               16, 16, 8);
+//    effectsAtlas = SpriteAtlas(ResourceManager::LoadTexture("assets/atlas/atlas_effects.png"),
+//                               16, 16);
 
-    objectsAtlas = SpriteAtlas(ResourceManager::LoadTexture("assets/atlas/atlas_objects.png", true, ""),
-                               16, 16, 19);
+//    objectsAtlas = SpriteAtlas(ResourceManager::LoadTexture("assets/atlas/atlas_objects.png"),
+//                               16, 16);
+
+    cardAtlas = SpriteAtlas(ResourceManager::LoadTexture("assets/atlas/atlas_test.png"),
+                            60, 84);
 
     renderer2D = new Renderer2D();
 //    coin = new Sprite("assets/sprites/coin.png", true);
@@ -81,13 +86,23 @@ void Game::update(float deltaTime)
 
 void Game::render()
 {
+    // FIXME: Rendering sprites from two different textures make the "second" completely black.
+    //  I think this is because of the 'AcctiveTexture' flag setting on OpenGL. The way to solve this is to
+    //  implement a Sprite Batch and probably making a dirty flag
+
     // Cursor Test
-    renderer2D->drawCursor(Input::getMousePosition(), glm::vec2(32.0f, 32.0f));
+//    renderer2D->drawCursor(Input::getMousePosition(), glm::vec2(32.0f, 32.0f));
 
     // Sprites Animation Test
 //    renderer2D->drawSprite(marioAtlas.getSprite(marioAnimIdx), glm::vec2(0.0f, 0.0f), glm::vec2(48.0f, 96.0f));
 //    renderer2D->drawSprite(objectsAtlas.getSprite(coinAnimIdx), glm::vec2(64.0f, 0.0f), glm::vec2(48.0f, 48.0f));
 //    renderer2D->drawSprite(objectsAtlas.getSprite(blockAnimIdx), glm::vec2(128.0f, 0.0f), glm::vec2(48.0f, 48.0f));
+
+    // Cards
+    renderer2D->drawSprite(cardAtlas.getSprite(0), glm::vec2(0.0f, 0.0f), 1.0f);
+    renderer2D->drawSprite(cardAtlas.getSprite(1), glm::vec2(100.0f, 0.0f), 1.0f);
+    renderer2D->drawSprite(cardAtlas.getSprite(2), glm::vec2(200.0f, 0.0f), 1.0f);
+    renderer2D->drawSprite(cardAtlas.getSprite(3), glm::vec2(300.0f, 0.0f), 1.0f);
 
 //    renderer2D->drawQuad(glm::vec2(0.0f, 0.0f), glm::vec2(100.0f, 100.0f), Color::Cyan);
 //    renderer2D->drawSprite(ResourceManager::GetTexture("mario_atlas"), glm::vec2(0.0f, 0.0f), glm::vec2(544.0f, 416.0f));
