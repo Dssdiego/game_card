8d966948172ab182620e8fc5050f3342519dbc56
Diego S. Seabra <dss-diego@hotmail.com>
2021-04-27 20:59:44 -0300

    time class implementation

---
+++ src/engine/time.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Diego Santos Seabra on 20/04/21.
+//
+
+#include "time.h"
+
+uint32_t Time::delta;
---
+++ src/engine/time.h
@@ -0,0 +1,15 @@
+//
+// Created by Diego Santos Seabra on 20/04/21.
+//
+
+#ifndef MARIO_TIME_H
+#define MARIO_TIME_H
+
+#include <cstdint>
+
+namespace Time
+{
+    extern uint32_t delta;
+}
+
+#endif //MARIO_TIME_H
